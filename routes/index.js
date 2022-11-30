8cad28384d77c4e61be8fe1ae13d593c0d0e5b62
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-11-29 18:09:55 -0700

    fixed getting notes bug

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const apiRoutes = require('./api')
 
 
+router.get("/", (req, res) =>
+	res.sendFile(path.join("./public/index.html"))
+);
 
 router.get("/notes", (req, res) =>
 	res.sendFile(path.join(__dirname, "../public/notes.html"))
--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-	res.sendFile(path.join("./public/index.html"))
-);
+
 
 app.use(routes);
 
