becaa02da853e5a8d04b837a89324f3f35781181
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-12-08 18:03:42 -0700

    heroku testing

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require('./api')
 
 
-router.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 router.get("/notes", (req, res) =>
 	res.sendFile(path.join(__dirname, "../public/notes.html"))
